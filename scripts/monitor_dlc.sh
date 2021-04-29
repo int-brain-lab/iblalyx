74e1daab0aa73088e0ef5bcb3deef356f8d31771
Ubuntu <ubuntu@ip-172-31-45-214.eu-west-2.compute.internal>
2021-04-29 20:34:18 +0000

    DLC monitoring jobs

--- scripts/monitor_dlc.sh
+++ scripts/monitor_dlc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source ~/alyxvenv/bin/activate
-/var/www/alyx-main/alyx/manage.py shell < ~/qc_table.py >> ˜/dlc_remaining.txt
+/var/www/alyx-main/alyx/manage.py shell < ~/iblalyx/scripts/monitor_dlc.py >> ~/dlc_remaining.txt
