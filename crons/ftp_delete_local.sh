8cb95e0cdb0c4ae7bc91701144395f2106eda7d9
Ubuntu <ubuntu@ip-172-31-45-214.eu-west-2.compute.internal>
2021-05-04 13:10:19 +0000

    set the paths correctly

--- crons/ftp_delete_local.sh
+++ crons/ftp_delete_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source ~/alyxvenv/bin/activate
-/var/www/alyx-main/alyx/manage.py shell < ~/iblalyx/scripts/ftp_delete_local.py
+/var/www/alyx-main/alyx/manage.py shell < ~/iblalyx/crons/ftp_delete_local.py
--- crons/monitor_dlc.sh
+++ crons/monitor_dlc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source ~/alyxvenv/bin/activate
-/var/www/alyx-main/alyx/manage.py shell < ~/iblalyx/scripts/monitor_dlc.py >> ~/dlc_remaining.txt
+/var/www/alyx-main/alyx/manage.py shell < ~/iblalyx/crons/monitor_dlc.py >> ~/dlc_remaining.txt
