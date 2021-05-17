0c6cf187476e300bcbe459641934eb04d38dcae3
olivier <olivier.winter@hotmail.fr>
2021-05-17 09:38:29 +0100

    dlc monitor

--- crons/monitor_dlc.py
+++ crons/monitor_dlc.py
@@ -4,14 +4,9 @@ import json
 from jobs.models import Task
 from django.db.models import Avg, Count
 # status - 20: Waiting / 25: Held / 30: Started / 40: Errored / 45: Abandoned / 50: Empty / 60: Complete
-
-
+DCHOICES = {c[0]:c[1] for c in Task.status.field.choices}
 count = Task.objects.filter(name='EphysDLC').values('status').annotate(n=Count('status'))
 
-d = {}
-for c in count:
-    k = next(ch[1] for ch in Task.status.field.choices if ch[0] == c['status'])
-    d[k] = c['n']
-
+d = {DCHOICES[c['status']]:c['n'] for c in count}
 t = datetime.now()
 print(t, json.dumps(d))
