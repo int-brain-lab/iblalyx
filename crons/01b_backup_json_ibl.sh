48ac4c39c8c12b7e6782b1c5eba9adcaa82e67cc
Ubuntu <ubuntu@ip-172-31-45-214.eu-west-2.compute.internal>
2021-09-22 11:04:15 +0000

    add upload to backup script

--- crons/01b_backup_json_ibl.sh
+++ crons/01b_backup_json_ibl.sh
@@ -8,8 +8,8 @@ python /var/www/alyx-main/alyx/manage.py dumpdata -e contenttypes -e auth.permis
 gzip -f "$backup_dir/alyx_full.json"
 
 # Send the files to the FlatIron server
-# scp ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -P 62022 "$backup_dir/alyx_full.json.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/alyxfull.json.gz
-rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" "$backup_dir/alyx_full.json.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/alyxfull.json.gz
+rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" "$backup_dir/alyx_full.sql.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/$(date  +%Y-%m-%d)_alyxfull.sql.gz
+
 
 # clean up the backups on AWS instance
 python /var/www/alyx-main/scripts/deployment_examples/99_purge_duplicate_backups.py
