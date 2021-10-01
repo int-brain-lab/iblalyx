28c27ba72cc704b1f4f4d6ea1f8df4bc88c363cc
olivier <olivier.winter@hotmail.fr>
2021-10-01 09:02:24 +0100

    update backup scripts

--- crons/01b_backup_json_ibl.sh
+++ crons/01b_backup_json_ibl.sh
@@ -1,15 +1,27 @@
 backup_dir="/backups/alyx-backups/$(date +%Y-%m-%d)"
 
+# Full django JSON dump, used by datajoint
 mkdir -p "$backup_dir"
-
 # Full django JSON dump.
 source /var/www/alyx-main/venv/bin/activate
-python /var/www/alyx-main/alyx/manage.py dumpdata -e contenttypes -e auth.permission -e reversion.version -e reversion.revision -e admin.logentry --indent 1 > "$backup_dir/alyx_full.json"
+python /var/www/alyx-main/alyx/manage.py dumpdata \
+    -e contenttypes -e auth.permission \
+    -e reversion.version -e reversion.revision -e admin.logentry \
+    -e actions.ephyssession \
+    -e actions.notification \
+    -e actions.notificationrule \
+    -e actions.virusinjection \
+    -e data.download \
+    -e experiments.brainregion \
+    -e jobs.task \
+    -e misc.note \
+    -e subjects.subjectrequest \
+    --indent 1 -o "alyx_full.json"
+
 gzip -f "$backup_dir/alyx_full.json"
 
 # Send the files to the FlatIron server
-rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" "$backup_dir/alyx_full.sql.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/$(date  +%Y-%m-%d)_alyxfull.sql.gz
-
+rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" "$backup_dir/alyx_full.json.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/alyxfull.json.gz
 
 # clean up the backups on AWS instance
 python /var/www/alyx-main/scripts/deployment_examples/99_purge_duplicate_backups.py
