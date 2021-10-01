2143268f2e62d8ab23dd600ee7648b1a07463e33
olivier <olivier.winter@hotmail.fr>
2021-10-01 09:22:40 +0100

    SQL dump add environment before running backup purge

--- crons/01_backup_ibl.sh
+++ crons/01_backup_ibl.sh
@@ -7,4 +7,5 @@ gzip -f "$backup_dir/alyx_full.sql"
 rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" "$backup_dir/alyx_full.sql.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/$(date +%Y-%m-%d)_alyxfull.sql.gz
 
 # clean up the backups on AWS instance
+source /var/www/alyx-main/venv/bin/activate
 python /var/www/alyx-main/scripts/deployment_examples/99_purge_duplicate_backups.py
