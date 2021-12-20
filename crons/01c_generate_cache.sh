410fa7c0050671949e29dc78a0b76eb148ab9a02
Miles Wells <k1o0@3tk.co>
2021-12-20 14:12:20 +0200

    Increase verbosity

--- crons/01c_generate_cache.sh
+++ crons/01c_generate_cache.sh
@@ -1,7 +1,7 @@
 # NB This is run on a cron job
 # Alyx cache tables
 source /var/www/alyx-main/venv/bin/activate
-python /var/www/alyx-main/alyx/manage.py one_cache --int-id
+python /var/www/alyx-main/alyx/manage.py one_cache --int-id -v 2
 
 source /var/www/alyx-dev/venv/bin/activate
-python /var/www/alyx-dev/alyx/manage.py one_cache --int-id
+python /var/www/alyx-dev/alyx/manage.py one_cache --int-id -v 2
