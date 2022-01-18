ec52903f5d8b058bf34956d45d55cb3213217114
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2022-01-18 11:25:19 +0100

    accounting for fact that insertion disappeared from Gsheet in Alyx

--- management/commands/_ibl/ONE_OFF__histology_sheet_assigndata_to_alyx.py
+++ management/commands/_ibl/ONE_OFF__histology_sheet_assigndata_to_alyx.py
@@ -34,9 +34,11 @@ data_sheet = data_sheet.rename(columns=data_sheet.iloc[0]).drop(data_sheet.index
 data_sheet = data_sheet[data_sheet['sess_id'] != ""]
 
 for pid, lab_assigned in zip(data_sheet.ins_id, data_sheet.assign_lab):
-    pi = ProbeInsertion.objects.filter(id=pid)[0]
-    # Get json and change it
-    d = pi.json
-    d['todo_alignment'] = lab_assigned
-    pi.json = d
-    pi.save()
+    pi = ProbeInsertion.objects.filter(id=pid)
+    if len(pi) > 0:
+        pi = pi[0]
+        # Get json and change it
+        d = pi.json
+        d['todo_alignment'] = lab_assigned
+        pi.json = d
+        pi.save()
