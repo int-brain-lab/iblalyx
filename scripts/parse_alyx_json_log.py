a03cf88e17f382fd989f3b461f031a1ba53cca92
Michele Fabbri <michele.fabbri@internationalbrainlab.org>
2022-01-26 09:44:09 +0000

    output to csv

--- scripts/parse_alyx_json_log.py
+++ scripts/parse_alyx_json_log.py
@@ -1,10 +1,6 @@
-import re
 import json
 import pandas as pd
-
-
-# TODO:
-#   - output as csv file
+import re
 
 FLAGS = re.VERBOSE | re.MULTILINE | re.DOTALL
 WHITESPACE = re.compile(r'[ \t\n\r]*', FLAGS)
@@ -20,10 +16,21 @@ def next_json(s):
     return obj, s[end:]
 
 
-def load_json_log_file(json_file_location):
+def load_json_log_file(json_file_loc):
+    """Loads the JSON file from the specified file location
 
+    Parameters
+    ----------
+    json_file_loc : string
+        Location of the JSON file to be evaluated
+
+    Returns
+    -------
+    pd.DataFrame
+        Datasets frame
+    """
     # Open json file for evaluation
-    with open(json_file_location) as json_file:
+    with open(json_file_loc) as json_file:
         json_string = json_file.read()
 
     # Read every json chunk
@@ -39,7 +46,8 @@ def load_json_log_file(json_file_location):
             url = parts[2][1:-1]
             endpoint = url.split('/')[1].split('?')[0]
             # append all info
-            data.append([obj['level'], obj['ip'], obj['timestamp'], verb, endpoint, url, obj['request']])
+            data.append(
+                [obj['level'], obj['ip'], obj['timestamp'], verb, endpoint, url, obj['request']])
 
         # Check if this is a 'duplicate' entry
         elif obj['event'] == 'request_finished':
@@ -63,79 +71,13 @@ def load_json_log_file(json_file_location):
 
 
 if __name__ == '__main__':
-    # Loading alyx_json file
-    # file_location = "/home/user/Documents/IBL/alyx_json_partial.log"
-    file_location = "/var/log/alyx_json.log"
-    load_json_log_file(file_location)
+    # Set the working directory and file locations
+    working_directory = "/var/log/"
+    json_file_location = working_directory+"alyx_json.log"
+    csv_out_file_location = working_directory + "alyx_json_out.csv"
 
-    # Code below left in case we are going to be looking to parse out access_alyx-main.log
-    # Loading of access_alyx-main log
-    # file_location = "/home/user/Documents/IBL/access_alyx-main.log"
-    # df = pd.DataFrame(columns=[
-    #     'ip',
-    #     'datetime',
-    #     'verb_type',
-    #     'endpoint',
-    #     'url'])
-    # with open(file_location) as file:
-    #     for line in file.readlines():
-    #         parsed_ip_address = ""
-    #         parsed_datetime = ""
-    #         parsed_verb_type = ""
-    #         parsed_endpoint = ""
-    #         parsed_url = ""
-    #         parsed_list = []
-    #         for part in line.split():
-    #             try:
-    #                 # Get IP Address
-    #                 if not parsed_ip_address:
-    #                     parsed_ip_address = ipaddress.ip_address(part).compressed
-    #                     parsed_list.append(parsed_ip_address)
-    #                 # Get datetime
-    #                 elif not parsed_datetime:
-    #                     parsed_datetime = datetime.strptime(
-    #                         part, "[%d/%b/%Y:%X").strftime("%Y-%m-%d:%X")
-    #                     parsed_list.append(parsed_datetime)
-    #                 # Get verb type
-    #                 elif not parsed_verb_type:
-    #                     if '"GET' in part:
-    #                         parsed_verb_type = 'GET'
-    #                     elif '"PATCH' in part:
-    #                         parsed_verb_type = 'PATCH'
-    #                     elif '"POST' in part:
-    #                         parsed_verb_type = 'POST'
-    #                     if parsed_verb_type:
-    #                         parsed_list.append(parsed_verb_type)
-    #                 # Get parsed_endpoint
-    #                 elif not parsed_endpoint:
-    #                     if part.startswith('/'):
-    #                         parsed_endpoint = part
-    #                         parsed_list.append(parsed_endpoint)
-    #                 # Get parsed_url
-    #                 elif not parsed_url:
-    #                     if part.startswith('"https://'):
-    #                         parsed_url = part
-    #                         parsed_list.append(parsed_url)
-    #             except ValueError:
-    #                 pass
-    #         # end of part for loop
-    #         if not parsed_url:
-    #             # URL was never set b/c log likely does not include it
-    #             parsed_url = "N/A"
-    #             parsed_list.append(parsed_url)
-    #
-    #         row = pd.Series(parsed_list, index=df.columns)
-    #         print(row)
-    #         df = df.append(row, ignore_index=True)
-    #
-    #         # df.loc[len(df)] = parsed_list
-    #         # newDF.append(pd.Series(new_row, index=newDF.columns[:len(new_row)]), ignore_index=True)
-    #         # df.append(
-    #         #     pd.Series(parsed_list, index=df.columns[:len(parsed_list)]), ignore_index=True)
-    #
-    # # Save to the data frame/series?
-    # # for key, value in parser_dict.items():
-    # #     print(key, ':', value)
-    # # print(df)
-    # #
-    # print(df)
+    # Parse out relevant data from alyx_json file, store to dataframe
+    dataframe = load_json_log_file(json_file_location)
+
+    # Output dataframe to CSV file
+    dataframe.to_csv(csv_out_file_location)
