0456aae9a3417029aa1955e0081de31279cdd696
Michele Fabbri <micheleangelofabbri@protonmail.com>
2022-01-26 15:27:46 +0000

    added date checking, output file with date

--- crons/parse_alyx_json_log.py
+++ crons/parse_alyx_json_log.py
@@ -1,6 +1,9 @@
+import datetime
+import fastparquet  # required for to_parquet function
 import json
 import pandas as pd
 import re
+import time
 
 FLAGS = re.VERBOSE | re.MULTILINE | re.DOTALL
 WHITESPACE = re.compile(r'[ \t\n\r]*', FLAGS)
@@ -38,24 +41,30 @@ def load_json_log_file(json_file_loc):
     while len(json_string) > 0:
         # Read the next json chunk
         obj, remaining = next_json(json_string)
-        # Check if this is a 'duplicate' entry
-        if obj['event'] == 'request_started':
-            # Parse out request type verb (get, patch, post), endpoint and full request url
-            parts = obj['request'][1:-1].split()
-            verb = parts[1]
-            url = parts[2][1:-1]
-            endpoint = url.split('/')[1].split('?')[0]
-            # append all info
-            data.append(
-                [obj['level'], obj['ip'], obj['timestamp'], verb, endpoint, url, obj['request']])
 
-        # Check if this is a 'duplicate' entry
-        elif obj['event'] == 'request_finished':
-            pass
+        # Look at only logs for today
+        if obj['timestamp'][:10] == datetime.datetime.today().isoformat()[:10]:
+            # Check if this is a 'duplicate' entry
+            if obj['event'] == 'request_started':
+                # Parse out request type verb (get, patch, post), endpoint and full request url
+                parts = obj['request'][1:-1].split()
+                verb = parts[1]
+                url = parts[2][1:-1]
+                endpoint = url.split('/')[1].split('?')[0]
+                # append all info
+                data.append(
+                    [obj['level'], obj['ip'], obj['timestamp'], verb, endpoint, url,
+                     obj['request']])
 
-        # Encountered something unplanned, output to terminal
+            # Check if this is a 'duplicate' entry
+            elif obj['event'] == 'request_finished':
+                pass
+
+            # Encountered something unplanned, output to terminal
+            else:
+                print("unknown event", obj)
         else:
-            print("unknown event", obj)
+            print("current json chunk is not for today")
 
         # Move on to next part of the string
         json_string = remaining
@@ -77,7 +86,8 @@ if __name__ == '__main__':
     # Set the working directory and file locations
     working_directory = "/var/log/"
     json_file_location = working_directory + "alyx_json.log"
-    parquet_out_file_location = working_directory + "alyx_json_log.parquet"
+    parquet_out_file_location = working_directory + \
+        "alyx_daily_json_parquets/" + time.strftime("%Y-%m-%d") + "_alyx_json_log.parquet"
 
     # Parse out relevant data from alyx_json file, store to dataframe
     dataframe = load_json_log_file(json_file_location)
