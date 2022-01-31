da1c25f99a1b83220ea4523a72418a7e8bcabd7e
Ubuntu <ubuntu@ip-172-31-45-214.eu-west-2.compute.internal>
2022-01-31 10:38:14 +0000

    change globus path on histology server

--- crons/07_histology.sh
+++ crons/07_histology.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /var/www/alyx-main/venv/bin/activate
-globus transfer 3291ffa4-7ec6-11ec-9f32-ed182a728dff:/~/mnt/ibl/ ab2d064c-413d-11eb-b188-0ee0d5d9299f:/histology/ --recursive --sync-level mtime
+globus transfer 3291ffa4-7ec6-11ec-9f32-ed182a728dff:/mnt/ibl/ ab2d064c-413d-11eb-b188-0ee0d5d9299f:/histology/ --recursive --sync-level mtime
