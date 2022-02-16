9ea18acc51a267efc448701b0b6344ce77bf8f42
Michele Fabbri <michele.fabbri@internationalbrainlab.org>
2022-02-16 14:53:24 +0000

    moving dev db restore to dev rds instance

--- crons/01_backup_ibl.sh
+++ crons/01_backup_ibl.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
-set -e
+set -e  # exit immediately if a command exits with a non-zero status
+
+# DB vars
 HOST=alyx.clfrcwlvymbw.eu-west-2.rds.amazonaws.com
-backup_dir="/backups/alyx-backups/$(date +%Y-%m-%d)"
-mkdir -p "$backup_dir"
+USERNAME=ibl_dev
+DBNAME=alyx
 
-# Full SQL dump.
-/usr/bin/pg_dump -cOx -U ibl_dev -h $HOST -d alyx -f "$backup_dir/alyx_full.sql"
-gzip -f "$backup_dir/alyx_full.sql"
-rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" "$backup_dir/alyx_full.sql.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/$(date +%Y-%m-%d)_alyxfull.sql.gz
+# Filesystem vars
+BACKUP_DIR="/backups/alyx-backups/$(date +%Y-%m-%d)"
+BACKUP_FILE_NAME=alyx_full.tar
+COMPRESSED_FILE_NAME="$BACKUP_FILE_NAME.gz"
 
-# the backup will be used later on by the dev reset job
+# Create the backup directory and set as working directory
+mkdir -p "$BACKUP_DIR"; cd "$BACKUP_DIR"
+
+# SQL dump to archive file
+pg_dump --host=$HOST \
+        --username=$USERNAME \
+        --dbname=$DBNAME \
+        --no-acl \
+        --format=tar \
+        --file=$BACKUP_FILE_NAME
+
+# Compress tar for rsync to flatiron, file is used daily for resetting the dev db with the prod db
+tar -czvf $COMPRESSED_FILE_NAME $BACKUP_FILE_NAME
+# -c - Create a new archive
+# -z - Use gzip compression
+# -v - Provide verbose output
+# -f - Archive file name
+
+# Transfer compressed archive to flatiron with rsync
+rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" \
+  "$COMPRESSED_FILE_NAME" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/$(date +%Y-%m-%d)_alyxfull.tar.gz
--- crons/03_dev_reset.sh
+++ crons/03_dev_reset.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
-set -e
-HOST=alyx.clfrcwlvymbw.eu-west-2.rds.amazonaws.com
-backup_dir="/backups/alyx-backups/$(date +%Y-%m-%d)"
-mkdir -p "$backup_dir"
+set -e  # exit immediately if a command exits with a non-zero status
 
-# Delete the full dev database
-psql -q -U ibl_dev -h $HOST -d alyx_dev -c "drop schema public cascade"
-psql -q -U ibl_dev -h $HOST -d alyx_dev -c "create schema public"
-# unzip the sql
-gunzip -k $backup_dir/alyx_full.sql.gz
-# Loads the main database into the dev one
-psql -h $HOST -U ibl_dev -d alyx_dev -f $backup_dir/alyx_full.sql
-rm $backup_dir/alyx_full.sql
+# DB vars
+HOST=alyx-dev.clfrcwlvymbw.eu-west-2.rds.amazonaws.com
+USERNAME=alyx-dev
+DBNAME=alyx-dev
+
+# Filesystem vars
+BACKUP_DIR="/backups/alyx-backups/$(date +%Y-%m-%d)"
+BACKUP_FILE_NAME=alyx_full_test.tar
+COMPRESSED_FILE_NAME="$BACKUP_FILE_NAME.gz"
+
+# Create the backup directory and set as working directory in case it does not already exist (this would be a problem)
+mkdir -p "$BACKUP_DIR"; cd "$BACKUP_DIR"
+
+# uncompress the archive
+tar -xzvf $COMPRESSED_FILE_NAME
+# -z - Use gzip compression
+# -v - Provide verbose output
+# -f - Archive file name
+# -x - Extract from a compressed file
+
+# Drop/delete the public schema
+psql --quiet \
+     --host=$HOST \
+     --username=$USERNAME \
+     --dbname=$DBNAME \
+     --command="drop schema public cascade"
+
+# Recreate the public schema
+psql --quiet \
+     --host=$HOST \
+     --username=$USERNAME \
+     --dbname=$DBNAME \
+     --command="create schema public"
+
+# Loads the main database backup into the dev environment
+pg_restore --host=$HOST \
+           --username=$USERNAME \
+           --dbname=$DBNAME \
+           --no-owner \
+           --no-acl \
+           --schema=public \
+           $BACKUP_FILE_NAME
+
+# Remove the non-compressed file now that we have loaded it into the dev db
+rm $BACKUP_FILE_NAME
+
 # Mirror the migrations from alyx-main to alyx-dev: wipe them all out and then copy from alyx-main
 rm -r /var/www/alyx-dev/alyx/*/migrations/0*
 cp -r /var/www/alyx-main/alyx/misc/migrations/0* /var/www/alyx-dev/alyx/misc/migrations/
 cp -r /var/www/alyx-main/alyx/data/migrations/0* /var/www/alyx-dev/alyx/data/migrations/
 cp -r /var/www/alyx-main/alyx/actions/migrations/0* /var/www/alyx-dev/alyx/actions/migrations/
 cp -r /var/www/alyx-main/alyx/subjects/migrations/0* /var/www/alyx-dev/alyx/subjects/migrations/
-# Apply migrations (if any)
+
+# Apply django migrations (if any)
 cd /var/www/alyx-dev/alyx
 source ../venv/bin/activate
 ./manage.py makemigrations
