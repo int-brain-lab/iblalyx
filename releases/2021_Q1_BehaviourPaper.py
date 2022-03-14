0b54d6b2d0fa501817d3107216207fd28a0df6b5
juhuntenburg <ju.huntenburg@gmail.com>
2022-03-14 13:11:13 +0000

    Change Behaviour paper to trials.table

--- releases/2021_Q1_BehaviourPaper.py
+++ releases/2021_Q1_BehaviourPaper.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from django.db.models import Q
 from data.models import Tag, Dataset, DatasetType
+from actions.models import Session
 
 # Releases as part of paper The International Brain Laboratory et al, 2021, DOI: 10.7554/eLife.63711
 
 # Original query based on list of session ids
 eids = list(pd.read_csv('2021_Q1_BehaviourPaper_sessions.csv', index_col=0)['session_id'])
+
+# First get the trials.table datasets for all session that they are available for
+tables_ds = Dataset.objects.filter(session__in=eids, dataset_type__name='trials.table')
+# For the rest of the sessions, get the individual datasets
+sess_no_table = Session.objects.filter(pk__in=eids).filter(~Q(pk__in=tables_ds.values_list('session_id')))
 dtypes = [
             'trials.feedback_times',
             'trials.feedbackType',
@@ -15,11 +22,16 @@ dtypes = [
             'trials.contrastRight',
             'trials.probabilityLeft',
             'trials.stimOn_times',
-            'trials.repNum',
             'trials.goCue_times',
             ]
 dataset_types = DatasetType.objects.filter(name__in=dtypes)
-dsets = Dataset.objects.filter(session__in=eids, dataset_type__in=dataset_types)
+indv_ds = Dataset.objects.filter(session__in=sess_no_table, dataset_type__in=dataset_types)
+
+# For all sessions, get the reNum data (not included in trials.table)
+repnum_ds = Dataset.objects.filter(session__in=eids, dataset_type__name='trials.repNum')
+
+# Bring all datasets together
+dsets = tables_ds | indv_ds | repnum_ds
 
 # Tagging in production database
 tag, _ = Tag.objects.get_or_create(name="Behaviour Paper", protected=True, public=True)
