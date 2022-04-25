2577576eb679d4229c36a68278335d9d09e446b3
juhuntenburg <ju.huntenburg@gmail.com>
2022-04-25 12:03:40 +0100

    Update to release process

--- releases/01_create_public_db.sh
+++ releases/01_create_public_db.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 set -e
 
-# This script (re-)create the public database through the following steps:
-# 1. Destroy the public database
-# 2. Replace the public database with the production database
-# 3. Prune the public database to contain only the to-be-released information
+# This script creates a local version of the public database through the following steps:
+# 1. Destroy the local instance of the public database
+# 2. Load the latest sql dump of the production database into the local public database
+# 3. Prune the local public database to contain only the to-be-released information
 
 # Consult the Alyx playbook before running this, as it requires some careful setup. Minimally:
 # * Make sure you have the correct database set up as 'public' in the alyx installation you use
 # * Make sure all involved databases are on the same branch and commit (production, public, local install)
-# * Make sure you have the password for the postgres user on the openalyx RDS instance (Keepass), you need to enter this three times
-# * Set the maintenance trigger on the public alyx instance
-# * Record the output of this script in a log file and submit these logs to github for the record
-# * Adjust the variables below according to your setup
+# * Record the output of this script in a log file (and submit these logs to github for the record later)
+# * Adjust the variables below according to your setup (any changes here should also be made in 02_upload_public_db.sh)
 
 # Variables
-OPENALYX_RDS=openlayx.clfrcwlvymbw.eu-west-2.rds.amazonaws.com  # The address of the openalyx database
 TMP_DIR="$HOME"/openalyx_wd  # This will be recreated and later destroyed, make sure you have write access
 ALYX_DIR=/var/www/alyx-main/alyx # The alyx installation you are using with public db set up
 SSH_STR=(mbox)  # ssh/scp alias to connect to the mbox EC2. If you don't have an alias set up you can pass your
                 # the ssh/scp options as an array such as (-i ~/.ssh/my_key.pem ubuntu@mbox.internationalbrainlab.org)
 
-echo "$(date '+%Y-%m-%d') Beginning to create public database"
+echo "$(date '+%Y-%m-%d') Beginning to create local version of public database"
 # Create a working directory
 echo "... creating working directory $TMP_DIR"
 mkdir -p "$TMP_DIR"
@@ -32,16 +29,20 @@ gunzip -c "$TMP_DIR"/alyxfull.sql.gz > "$TMP_DIR"/alyxfull.sql
 
 # Source alyx env
 source $ALYX_DIR/alyxvenv/bin/activate
-echo "... destroying the public database"
-# Reset the public database
-psql -U postgres -h $OPENALYX_RDS -d public -c "drop schema public cascade"
-psql -U postgres -h $OPENALYX_RDS -d public -c "create schema public"
-# Load the production alyx sql dump to public db
-echo "... recreating public database from production"
-psql -U postgres -h $OPENALYX_RDS -d public -f "$TMP_DIR"/alyxfull.sql
-# Prune public database
-echo "... pruning public database"
+# Destroy local public database and load production database into it
+echo "... destroying local public database"
+python $ALYX_DIR/alyx/manage.py reset_db -D public --noinput
+echo "... rebuilding local public database from production"
+psql -U labdbuser -h localhost -d public -f "$TMP_DIR"/alyxfull.sql
+# Prune local public database
+echo "... pruning local public database"
 python $ALYX_DIR/alyx/manage.py shell < 01a_prune_public_db.py
-# Remove tmp directory
-rm -rf "$TMP_DIR"
-echo "Finished creating public database"
+# Export the public db to sql
+echo "... creating sql dump of local public database"
+/usr/bin/pg_dump -cOx -U labdbuser -h localhost -d public -f "$TMP_DIR"/openalyx.sql
+gzip -k "$TMP_DIR"/openalyx.sql
+## Copy sql as backup to mbox
+echo "... copying sql dump to mbox as backup"
+scp "$TMP_DIR"/openalyx.sql.gz "${SSH_STR[@]}":/backups/openalyx-backups/$(date +%Y-%m-%d)_openalyx.sql.gz
+
+echo "Finished creating local version of public database"
