37fc6151c2da53c7af1441b769dade2ddad82e0b
k1o0 <k1o0@3tk.co>
2022-06-01 11:23:25 +0000

    sync cron executable; purge old after backup

--- crons/01_backup_ibl.sh
+++ crons/01_backup_ibl.sh
@@ -9,4 +9,6 @@ mkdir -p "$backup_dir"
 gzip -f "$backup_dir/alyx_full.sql"
 rsync -av --progress -e "ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022" "$backup_dir/alyx_full.sql.gz" alyx@ibl.flatironinstitute.org:/mnt/ibl/json/$(date +%Y-%m-%d)_alyxfull.sql.gz
 
-# the backup will be used later on by the dev reset job
+# clean up the backups on AWS instance
+source /var/www/alyx-main/venv/bin/activate
+python /var/www/alyx-main/scripts/deployment_examples/99_purge_duplicate_backups.py
