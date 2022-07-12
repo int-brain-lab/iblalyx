43fdf91d991a389045bfb9fe113ea0216f4271c3
Miles Wells <k1o0@3tk.co>
2022-07-12 14:10:29 +0300

    Renamed alyxdev to alyxtest

--- crons/alyx-test/00_rebuild_from_cache.sh
+++ crons/alyx-test/00_rebuild_from_cache.sh
@@ -5,10 +5,10 @@ whoami
 psql -q -U ibl_user -h localhost -d ibl_test -c "drop schema public cascade"
 psql -q -U ibl_user -h localhost -d ibl_test -c "create schema public"
 # Loads the main database into the dev one
-psql -h localhost -U ibl_user -d ibl_test -f /var/www/alyx-dev/data/alyx_full.sql
+psql -h localhost -U ibl_user -d ibl_test -f /var/www/alyx-test/data/alyx_full.sql
 # activate environment
-source /home/ubuntu/alyxvenv/bin/activate
-cd /var/www/alyx-dev/alyx
+source /var/www/alyx-test/venv/bin/activate
+cd /var/www/alyx-test/alyx
 # django migrations
 ./manage.py makemigrations
 ./manage.py migrate
--- crons/alyx-test/01_write_db_to_cache.sh
+++ crons/alyx-test/01_write_db_to_cache.sh
@@ -1,1 +1,1 @@
-/usr/bin/pg_dump -cOx -U ibl_user -h localhost -d ibl_test -f /var/www/alyx-dev/data/alyx_full.sql
+/usr/bin/pg_dump -cOx -U ibl_user -h localhost -d ibl_test -f /var/www/alyx-test/data/alyx_full.sql
