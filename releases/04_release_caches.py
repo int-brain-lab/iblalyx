68d574d6107b1d10a23cd24ebb2f7b18769650b5
Miles Wells <k1o0@3tk.co>
2022-07-21 16:50:36 +0300

    Generate one cache per release

---
+++ releases/04_release_caches.py
@@ -0,0 +1,16 @@
+"""
+For each release tag in OpenAlyx, generate a separate set of parquet tables on S3 containing
+only those sessions/datasets
+"""
+import urllib.parse
+
+from alyx.settings import TABLES_ROOT
+from data.models import Tag
+import misc.management.commands.one_cache as cache
+
+assert TABLES_ROOT[-1] != '/'
+
+for tag in Tag.objects.all().values_list('name', flat=True):
+    print(f'Generating cache for release "{tag}"')
+    cmd = cache.Command()
+    cmd.handle(tag=tag, compress=True, dst_dir=TABLES_ROOT + '/' + urllib.parse.quote(tag))
