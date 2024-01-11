aae5179fec6a8795ca9e6d2e46f85b327f055fc4
Miles Wells <k1o0@5tk.co>
2024-01-11 13:42:02 +0200

    Add health check to process logs

--- crons/process_logs.sh
+++ crons/process_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022 datauser@ibl.flatironinstitute.org "source ~/Documents/PYTHON/envs/iblenv/bin/activate; python ~/Documents/github/iblalyx/scripts/process_logs.py $1 $2 > ~/s3_logs/s3_log.log 2>&1"
+ssh -i /home/ubuntu/.ssh/sdsc_alyx.pem -p 62022 datauser@ibl.flatironinstitute.org "source ~/Documents/PYTHON/envs/iblenv/bin/activate; python ~/Documents/github/iblalyx/scripts/process_logs.py $1 $2 > ~/s3_logs/s3_log.log 2>&1" && curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/d067db71-0783-475e-8159-cf81fee57c6b
