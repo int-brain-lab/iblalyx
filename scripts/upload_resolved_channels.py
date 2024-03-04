4bfe721bd2346e9b818446a3161f1917eacf9322
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2024-03-04 11:22:55 +0000

    reupload alignments that have been manually resolved

--- scripts/upload_resolved_channels.py
+++ scripts/upload_resolved_channels.py
@@ -1,9 +1,10 @@
 from ibllib.pipes.tasks import Task
 from ibllib.pipes.ephys_alignment import EphysAlignment
 import spikeglx
-from ibllib.atlas import AllenAtlas
+from iblatlas.atlas import AllenAtlas
 import one.alf.io as alfio
 import numpy as np
+from datetime import date, timedelta
 from one.api import ONE
 from experiments.models import ProbeInsertion
 from django.db.models import Count, Q
@@ -107,37 +108,48 @@ class RegisterChannels(Task):
         return files_to_register
 
 
+def upload_channels(ins):
+
+    for pr in ins:
+        try:
+            traj = list(pr.trajectory_estimate.order_by('-provenance').all()[0].json.keys())
+            traj.sort(reverse=True)
+            align_stored = pr.json['extended_qc']['alignment_stored']
+            if traj[0] != align_stored:
+                logger.warning(f'{pr.id} key mismatch: {align_stored} {traj}')
+                # Output to a logger
+                traj_align = next(t for t in traj if t.split('_')[1] in align_stored)
+                logger.info(f'Updating key for {pr.id} to {traj_align}')
+                pr.json['extended_qc']['alignment_stored'] = traj_align
+                pr.save()
+
+            eid, name = one.pid2eid(pr.id)
+            session_path = one.eid2path(eid)
+
+            task = RegisterChannels(session_path, one=one, pname=name, location='SDSC')
+            task.run()
+            response = task.register_datasets()
+            for resp in response:
+                fi = next((fr for fr in resp['file_records'] if 'flatiron' in fr['data_repository']), None)
+                if fi is not None:
+                    one.alyx.rest('files', 'partial_update', id=fi['id'], data={'exists': True})
+            task.cleanUp()
+        except Exception as err:
+            logger.error(f'{pr.id} errored with message: {err}')
+            task.cleanUp()
+
+
+# Find the insertions that have been resolved but don't yet have datasets
 ins = ProbeInsertion.objects.filter(json__extended_qc__alignment_resolved=True)
 ins = ins.annotate(n_chns=Count("datasets", filter=Q(
     datasets__name='channels.mlapdv.npy')))
 ins = ins.filter(n_chns__lt=1)
+logger.info(f'Uploading channels for newly resolved {ins.count()} insertions')
+upload_channels(ins)
 
-logger.info(f'Uploading channels for {ins.count()} insertions')
 
-for pr in ins:
-    try:
-        traj = list(pr.trajectory_estimate.order_by('-provenance').all()[0].json.keys())
-        traj.sort(reverse=True)
-        align_stored = pr.json['extended_qc']['alignment_stored']
-        if traj[0] != align_stored:
-            logger.warning(f'{pr.id} key mismatch: {align_stored} {traj}')
-            # Output to a logger
-            traj_align = next(t for t in traj if t.split('_')[1] in align_stored)
-            logger.info(f'Updating key for {pr.id} to {traj_align}')
-            pr.json['extended_qc']['alignment_stored'] = traj_align
-            pr.save()
-
-        eid, name = one.pid2eid(pr.id)
-        session_path = one.eid2path(eid)
-
-        task = RegisterChannels(session_path, one=one, pname=name, location='SDSC')
-        task.run()
-        response = task.register_datasets()
-        for resp in response:
-            fi = next((fr for fr in resp['file_records'] if 'flatiron' in fr['data_repository']), None)
-            if fi is not None:
-                one.alyx.rest('files', 'partial_update', id=fi['id'], data={'exists': True})
-        task.cleanUp()
-    except Exception as err:
-        logger.error(f'{pr.id} errored with message: {err}')
-        task.cleanUp()
+# Find the insertions that have already been resolved but have been manually newly resolved
+yesterday = (date.today() - timedelta(days=1)).isoformat()
+ins = ProbeInsertion.objects.filter(json__extended_qc__alignment_resolved_date__gte=yesterday)
+logger.info(f'Uploading channels for already resolved {ins.count()} insertions')
+upload_channels(ins)
