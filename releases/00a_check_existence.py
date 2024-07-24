4150777cfdfe562ae1fe429aea7a837140b59d38
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2024-07-24 10:13:09 +0100

    add tag to scripts

--- releases/00a_check_existence.py
+++ releases/00a_check_existence.py
@@ -30,7 +30,8 @@ public_ds_files = ['2021_Q1_IBL_et_al_Behaviour_datasets.pqt',
                    '2023_Q4_IBL_et_al_BWM_passive_datasets.pqt',
                    '2024_Q2_IBL_et_al_BWM_iblsort_datasets.pqt',
                    '2024_Q2_IBL_et_al_RepeatedSite_datasets.pqt',
-                   '2024_Q2_Blau_et_al_datasets.pqt'
+                   '2024_Q2_Blau_et_al_datasets.pqt',
+                   '2024_Q4_Pan_Vazquez_et_al.pqt'
                    ]
 
 # Select which release you want to check by changing i
@@ -188,7 +189,8 @@ public_ds_files = ['2021_Q1_IBL_et_al_Behaviour_datasets.pqt',
                    '2023_Q4_IBL_et_al_BWM_passive_datasets.pqt',
                    '2024_Q2_IBL_et_al_BWM_iblsort_datasets.pqt',
                    '2024_Q2_IBL_et_al_RepeatedSite_datasets.pqt',
-                   '2024_Q2_Blau_et_al_datasets.pqt'
+                   '2024_Q2_Blau_et_al_datasets.pqt',
+                   '2024_Q4_Pan_Vazquez_et_al.pqt'
                    ]
 
 # Chose release with i
