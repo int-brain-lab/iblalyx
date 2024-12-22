8b8f2ed53541fc90a63be3d6c13cffd5f3cda602
owinter <olivier.winter@hotmail.fr>
2024-12-22 21:16:05 +0000

    sync_patcher: deletes dataset on the s3 bucket

--- management/commands/sync_patcher.py
+++ management/commands/sync_patcher.py
@@ -1,4 +1,5 @@
-"""Script to query datasets that need to be synced from the s3 patcher to Flatiron.
+"""
+Script to query datasets that need to be synced from the s3 patcher to Flatiron.
 Currently expected to run on SDSC with access to /mnt/ibl Flatiron directory.
 
 This command is currently set up to run on the Flatiron datauser account. To install, set up a link
@@ -6,36 +7,50 @@ to the alyx data app management commands folder:
 >>> basedir="/home/datauser/Documents/PYTHON"
 >>> ln -s "$basedir/iblalyx/management/commands/sync_patcher.py"
 "$basedir/alyx/alyx/data/management/commands/sync_patcher.py"
+
+For the full list of options
+>>> python manage.py help sync_patcher
+
 Examples
 --------
-Run in dry mode
->>> python manage.py sync_patcher --dryrun > /home/datauser/ibl_logs/sync_patcher.log 2>&1
-Run script
->>> python manage.py sync_patcher
+The command has 2 modes: list, and sync.
+
+List the files to be synced and deleted: no action will be performed
+>>> python manage.py sync_patcher list
+Run in dry mode: this will still invoke AWS commands in dry mode, and no copy will occur
+>>> python manage.py sync_patcher sync --dryrun
+Run the synchronization
+>>> python manage.py sync_patcher sync > /home/datauser/ibl_logs/sync_patcher.log 2>&1
 Force the overwrite of already existing files on flatiron (Run with extreme caution!!!!!)
->>> python manage.py sync_patcher --force True
+>>> python manage.py sync_patcher sync --force True
 """
 
 import time
 from dateutil.relativedelta import relativedelta as rd
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE
 import logging
 from pathlib import Path
 
-from one.alf.files import add_uuid_string
+from one.alf.path import add_uuid_string
 from django.core.paginator import Paginator
 from django.core.management import BaseCommand
+from django.db.models import Count, Sum, Q
 
 from data.models import DataRepository, Dataset, FileRecord
+import iblutil.util
 
-logger = logging.getLogger('data.transfers').getChild('aws')
+logger = iblutil.util.setup_logger(__name__)
 
 
-def log_subprocess_output(pipe, log_function=logger.info, cmd=None):
-    for line in iter(pipe.readline, b''):
-        log_function(line.decode().strip())
-    if cmd is not None:
-        log_function(f'Original command: {cmd}')
+def run_aws_command(cmd):
+    logger.debug(' '.join(cmd))
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        logger.error(stderr.decode('utf-8'))
+        raise RuntimeError(f'Command {" ".join(cmd)} failed with return code {process.returncode}')
+    else:
+        logger.debug(stdout.decode('utf-8'))
 
 
 def format_seconds(seconds):
@@ -53,6 +68,9 @@ class Command(BaseCommand):
     _query = None
 
     def add_arguments(self, parser):
+        parser.add_argument('action', default='sync', choices=['list', 'sync'])
+        parser.add_argument('--session', default=None, type=str, help='Restrict datasets for a specific session')
+        parser.add_argument('--user', default=None, type=str, help='Restrict to datasets for a specific user')
         parser.add_argument('--batch-size', default=50_000, type=int,
                             help='Max number of datasets per batch')
         parser.add_argument('--limit', default=500_000, type=int,
@@ -63,59 +81,79 @@ class Command(BaseCommand):
                             help='Displays the operations that would be performed using the '
                                  'specified command without actually running them.')
 
+
     def handle(self, *_, **options):
         verbosity = options.pop('verbosity')
+        level = logging.INFO
         if verbosity < 1:
-            logger.setLevel(logging.WARNING)
-        elif verbosity == 1:
-            logging.getLogger().setLevel(logging.INFO)
-            logger.setLevel(logging.INFO)
+            level = logging.WARNING
         elif verbosity > 1:
-            logging.getLogger().setLevel(logging.DEBUG)
-            logger.setLevel(logging.DEBUG)
+            level = logging.DEBUG
+         # Diagnostic information
+        logger.setLevel(level)
+        logger.propagate = False
 
         dry = options.pop('dryrun')
         force = options.pop('force')
         t0 = time.time()
+        # list the files to be synced
         query_paginated = self.build_sync_query(**options)
-        self.sync(query_paginated, force=force, dry=dry)
-        logger.debug('Entire sync and update took ' + format_seconds(time.time() - t0))
-        # TODO delete old files from the patcher
-        query_paginated = self.build_delete_query(**options)
-        self.delete(query_paginated, dry=dry)
+        # perform the sync if necessary
+        if options.get('action') =='sync':
+            self.sync(query_paginated, force=force, dry=dry)
+            logger.info('Entire sync and update took ' + format_seconds(time.time() - t0))
 
     @staticmethod
-    def build_sync_query(**options) -> Paginator:
-        """Build a paginated queryset of datasets to sync"""
+    def log_dataset(qs):
+        # Aggregate qs per session: count and sum of filesize
+        session_stats = qs.values('session').annotate(
+            dataset_count=Count('id'),
+            total_size=Sum('file_size')
+        )
+        total_size = 0
+        for stat in session_stats:
+            logger.info(f"Session {stat['session']}: {stat['dataset_count']} datasets, "
+                        f"{stat['total_size'] / (1024 ** 3):.2f} GB total")
+            total_size += stat['total_size']
+        logger.info(f'{qs.count()} files are due to be processed, representing {total_size / (1024 ** 3):.2f} GB total')
+        if logger.isEnabledFor(logging.DEBUG):
+            for dset in qs:
+                logger.debug(f'{dset.session}, {dset}')
 
+
+    @staticmethod
+    def build_query(flat_iron_exists=False, **options) -> Paginator:
+        """Build a paginated queryset of datasets to sync"""
         # Find datasets
         batch_size = options.pop('batch_size')
-        limit = options.pop('limit')
-
         # Find all file records with a s3 patcher data repository
         frs = FileRecord.objects.filter(data_repository__name='s3_patcher')
         # Find the associated datasets
         dsets = frs.values_list('dataset', flat=True)
         # Find the flatiron file records for the specific datasets that have exists=False
+        query_kwargs = {}
+        if options.get('user'):
+            query_kwargs['dataset__created_by__username'] = options.get('user')
+        if options.get('session'):
+            query_kwargs['dataset__session'] = options.get('session')
         frs = FileRecord.objects.filter(dataset__in=dsets, data_repository__name__icontains='flatiron',
-                                        exists=False)
+                                        exists=flat_iron_exists, **query_kwargs)
         # Get the datasets that need to be synced
-        qs = Dataset.objects.filter(id__in=frs.values_list('dataset', flat=True))
-
+        qs = Dataset.objects.filter(id__in=frs.values_list('dataset', flat=True)).order_by('session__start_time')
+        # Find datasets
+        Command.log_dataset(qs)
         # Restrict to specific number of datasets
+        limit = options.pop('limit')
         if limit:
             qs = qs[:limit]
-
-        logger.info(f'{qs.count()} files are due to be synced')
-
         return Paginator(qs, batch_size)
 
     @staticmethod
-    def build_delete_query(**options) -> Paginator:
-        """Build a paginated queryset of datasets to delete from the s3 patcher. These are datasets that have
-        flatiron and aws file records with both set to exists=True"""
+    def build_sync_query( **options) -> Paginator:
+        """Build a paginated queryset of datasets to sync"""
+        logger.info('Querying for files to sync')
+        return Command.build_query(flat_iron_exists=False, **options)
 
-        return
 
     @staticmethod
     def sync(paginated_query, force=False, dry=False):
@@ -146,39 +184,22 @@ class Command(BaseCommand):
             current_qs = data.object_list
 
             for dset in current_qs:
-                fr_flatiron = FileRecord.objects.get(dataset=dset, data_repository__name__icontains='flatiron')
+                fr_flatiron = dset.file_records.get(dataset=dset, data_repository__name__icontains='flatiron')
                 lab_path = fr_flatiron.data_repository.globus_path
                 dset_path = str(add_uuid_string(fr_flatiron.relative_path, dset.id))
                 dst_file = ROOT + lab_path + dset_path
                 src_file = bucket_name + PATCHER_ROOT + lab_path + dset_path
-
                 if Path(dst_file).exists() and not force:
                     logger.info(f'Destination file for {str(dset.id)} already exists and will not be overwritten, '
                                 f'set force=True to overwrite')
                     continue
-
                 # Build up the aws command
-                cmd = ['aws', 's3', 'cp', src_file, dst_file, '--profile', 'ibladmin']
+                cmd = ['aws', 's3', 'cp', src_file, dst_file, '--profile', 'ibladmin', '--no-progress']
                 if dry:
                     cmd.append('--dryrun')
-                if logger.level > logging.DEBUG:
-                    log_fcn = logger.error
-                    cmd.append('--only-show-errors')  # Suppress verbose output
-                else:
-                    log_fcn = logger.debug
-                    cmd.append('--no-progress')  # Suppress progress info, estimated time, etc.
-                logger.debug(' '.join(cmd))
                 t0 = time.time()
-                process = Popen(cmd, stdout=PIPE, stderr=STDOUT)
-                return_code = process.wait()
-                if return_code != 0:
-                    with process.stderr:
-                        log_subprocess_output(process.stderr, logger.error, cmd=cmd)
-                    raise RuntimeError(f'Command {cmd} failed with return code {return_code}')
-                else:
-                    with process.stdout:
-                        log_subprocess_output(process.stdout, logger.info, cmd=cmd)
-
+                logger.info(f'{dset.session}, {dset.collection}, {dset.name}, {dset.created_by.username}')
+                run_aws_command(cmd=cmd)
                 if not dry:
                     exists = Path(dst_file).exists()
                     # Update the file record
@@ -191,7 +212,14 @@ class Command(BaseCommand):
                         if fr_aws is not None:
                             fr_aws.exists = False
                             fr_aws.save()
-                        # TODO should we delete any local server file records
+                        # this deletes the file records from the local servers and from the S3 patcher
+                        frs = dset.file_records.all().exclude(
+                            Q(data_repository__name__startswith='flatiron') |
+                            Q(data_repository__name__startswith='aws')
+                        )
+                        run_aws_command(cmd= ['aws', 's3', 'rm', src_file, '--profile', 'ibladmin'])
+                        logger.debug(f'Deleting file records {frs.values_list("data_repository__name")}')
+                        frs.delete()
                     else:
                         logger.error(f'File for {str(dset.id)} was not transferred')
 
