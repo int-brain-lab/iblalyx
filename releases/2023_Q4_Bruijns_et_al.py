458c975ddb94557f3b58a0002b7f8221fe7ec1c3
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2025-02-19 09:58:54 +0000

    scripts for bruijns and mohammadi

--- releases/2023_Q4_Bruijns_et_al.py
+++ releases/2023_Q4_Bruijns_et_al.py
@@ -22,10 +22,37 @@ subjects = ['CSHL045', 'CSHL047', 'CSHL049', 'CSHL051', 'CSHL052', 'CSHL053', 'C
             'ibl_witten_29', 'ibl_witten_32'
             ]
 
-subjects = Subject.objects.filter(nickname__in=subjects)
+"""""""""""""""
+Original code
+"""""""""""""""
 
-datasets = Dataset.objects.filter(name__in=['_ibl_subjectTrials.table.pqt', '_ibl_subjectTraining.table.pqt']
-                                  , object_id__in=subjects.values_list('pk', flat=True))
+# subjects = Subject.objects.filter(nickname__in=subjects)
+#
+# datasets = Dataset.objects.filter(name__in=['_ibl_subjectTrials.table.pqt', '_ibl_subjectTraining.table.pqt']
+#                                   , object_id__in=subjects.values_list('pk', flat=True))
+#
+# tag, _ = Tag.objects.get_or_create(name="2023_Q4_Bruijns_et_al", protected=True, public=True)
+# tag.datasets.set(datasets)
+#
+# # Save dataset IDs for release in public database
+# dset_ids = [str(eid) for eid in datasets.values_list('pk', flat=True)]
+# df = pd.DataFrame(dset_ids, columns=['dataset_id'])
+# df.to_parquet('./2023_Q4_Bruijns_et_al_datasets.pqt')
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Adapted code to include subjectSessions.table Feb 2025
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Load in the original datasets (note 2023_Q4_Bruijns_et_al_datasets.pqt was renamed to
+# 2023_Q4_Bruijns_et_al_datasets_v1.pqt in Feb 2025)
+
+orig_dsets = pd.read_parquet('./2023_Q4_Bruijns_et_al_datasets_v1.pqt')
+dsets = Dataset.objects.filter(id__in=orig_dsets['dataset_id'].values)
+
+subjects = Subject.objects.filter(nickname__in=subjects)
+session_dsets = Dataset.objects.filter(name='_ibl_subjectSessions.table.pqt', object_id__in=subjects.values_list('id', flat=True))
+datasets = dsets | session_dsets
 
 tag, _ = Tag.objects.get_or_create(name="2023_Q4_Bruijns_et_al", protected=True, public=True)
 tag.datasets.set(datasets)
