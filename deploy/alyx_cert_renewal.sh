cfa0d1bcdd3a93fe993e2c62a8b3dc67f348ffde
Miles Wells <k1o0@5tk.co>
2025-07-01 15:05:09 +0300

    Try self-signed SSL

--- deploy/alyx_cert_renewal.sh
+++ deploy/alyx_cert_renewal.sh
@@ -11,10 +11,18 @@ if [ "$APACHE_SERVER_NAME" = "localhost" ]; then
 fi
 
 # Fist check if the certificate files exist
-if [ ! -f /etc/letsencrypt/live/$1/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/$1/privkey.pem ] && [ -n $CERTBOT_SG ]; then
+if [ ! -f /etc/letsencrypt/live/$APACHE_SERVER_NAME/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/$APACHE_SERVER_NAME/privkey.pem ] && [ -n $CERTBOT_SG ]; then
     echo "SSL certificate files do not exist. Proceeding with certificate generation"
     # Deactivate SSL module if it is enabled
-    a2dismod ssl
+    # a2dismod ssl
+    echo "Generating self-signed SSL certificate for $APACHE_SERVER_NAME"
+    # Create directories if they do not exist
+    mkdir -p /etc/letsencrypt/live/$APACHE_SERVER_NAME
+    sudo openssl req -x509 -nodes -days 1 -newkey rsa:2048 \
+        -keyout /etc/letsencrypt/live/$APACHE_SERVER_NAME/apache-selfsigned.key \
+        -out /etc/letsencrypt/live/$APACHE_SERVER_NAME/apache-selfsigned.crt \
+        -subj "/C=GB/ST=London/L=London/O=IBL/OU=IT/CN=${APACHE_SERVER_NAME}" &&
+
     # Generate a new SSL certificate using certbot
     # TODO Hostname variable should be set in the environment
     # Start apache server
@@ -23,5 +31,5 @@ if [ ! -f /etc/letsencrypt/live/$1/fullchain.pem ] || [ ! -f /etc/letsencrypt/li
     
     # Restart apache server to apply the new certificate
     apache2ctl stop
-    a2enmod ssl
+    # a2enmod ssl
 fi
