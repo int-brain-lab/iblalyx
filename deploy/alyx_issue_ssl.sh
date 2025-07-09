944ec6ee4d9933e6395a4318bb746951152a870d
Miles Wells <k1o0@3tk.co>
2025-07-09 23:47:53 +0300

    Minor changes to cert issuing script

--- deploy/alyx_issue_ssl.sh
+++ deploy/alyx_issue_ssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script is used to renew the SSL certificate for the Alyx server if certificates do not exist
+# This script is used to create a new SSL certificate for the Alyx server if certificates do not exist
 # $APACHE_SERVER_NAME is the hostname, e.g. example.com, sub.example.com, or localhost
 
 if [ "$APACHE_SERVER_NAME" = "localhost" ]; then
@@ -11,10 +11,9 @@ if [ "$APACHE_SERVER_NAME" = "localhost" ]; then
 fi
 
 # Fist check if the certificate files exist
-if [ ! -f /etc/letsencrypt/live/$APACHE_SERVER_NAME/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/$APACHE_SERVER_NAME/privkey.pem ] && [ -n $CERTBOT_SG ]; then
+if { [ ! -f /etc/letsencrypt/live/$APACHE_SERVER_NAME/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/$APACHE_SERVER_NAME/privkey.pem ]; } && [ -n "$CERTBOT_SG" ]; then
     echo "SSL certificate files do not exist. Proceeding with certificate generation"
-    # Deactivate SSL module if it is enabled
-    # a2dismod ssl
+    # To get apache running for the certbot challenge we first create a temporary self-signed certificate
     echo "Generating self-signed SSL certificate for $APACHE_SERVER_NAME"
     # Create directories if they do not exist
     mkdir -p /etc/letsencrypt/live/$APACHE_SERVER_NAME
@@ -22,15 +21,13 @@ if [ ! -f /etc/letsencrypt/live/$APACHE_SERVER_NAME/fullchain.pem ] || [ ! -f /e
         -keyout /etc/letsencrypt/live/$APACHE_SERVER_NAME/apache-selfsigned.key \
         -out /etc/letsencrypt/live/$APACHE_SERVER_NAME/apache-selfsigned.crt \
         -subj "/C=GB/ST=London/L=London/O=IBL/OU=IT/CN=${APACHE_SERVER_NAME}" &&
-    # Generate a new SSL certificate using certbot
-    # TODO Hostname variable should be set in the environment
+
     # Start apache server
     apache2ctl start
     rm -rf /etc/letsencrypt/live/$APACHE_SERVER_NAME
-
+    # Generate a new SSL certificate using certbot
     /bin/bash /home/iblalyx/crons/renew_docker_certs.sh
     
-    # Restart apache server to apply the new certificate
+    # Restart apache server to apply the new certificate (NB: server started by docker-compose)
     apache2ctl stop
-    # a2enmod ssl
 fi
