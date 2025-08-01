f1b1f38ca6fbd5e5f7dac3b7112a7cbd5bbf0087
Olivier Winter <olivier.winter@hotmail.fr>
2025-08-01 12:10:49 +0200

    make sure we can only create symlinks for specific tags

--- releases/04_copy_data.sh
+++ releases/04_copy_data.sh
@@ -6,10 +6,6 @@ set -e
 
 ALYX_DIR="$HOME"/Documents/PYTHON/alyx/alyx
 
-# Activate alyx env
-echo "$(date '+%Y-%m-%d %H:%M:%S') Creating symlinks"
-# Create all missing symlinks in /mnt/ibl/public for the public database
-"$HOME"/Documents/PYTHON/alyx/alyxvenv/bin/python $ALYX_DIR/manage.py shell < 03a_symlinks.py
 # Sync to AWS public bucket
 echo "$(date '+%Y-%m-%d %H:%M:%S') Syncing to public S3 bucket data/"
 aws s3 sync "/mnt/ibl/public" s3://ibl-brain-wide-map-public/data --exclude "aggregates/*" --exclude "*.zip" --exclude ".*" --profile ibladmin --follow-symlinks --delete --no-progress --only-show-errors
