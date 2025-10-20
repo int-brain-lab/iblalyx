8b0cfea335b35768da3841ac507003aa56e1ff11
Olivier Winter <olivier.winter@hotmail.fr>
2025-10-20 16:04:05 +0100

    fix start docker postgress: should run from anywhere

--- crons/mbox/start_docker_postgres.sh
+++ crons/mbox/start_docker_postgres.sh
@@ -4,5 +4,5 @@ docker rm postgres-alyx-client
 set -e
 docker run -d -e POSTGRES_PASSWORD=postgres -e PGPASSFILE=/.pgpass -v /home/ubuntu/tmp:/backups --name postgres-alyx-client postgres:17
 
-cat .pgpass | docker exec -i postgres-alyx-client sh -c 'cat >.pgpass'
+cat ~/.pgpass | docker exec -i postgres-alyx-client sh -c 'cat >.pgpass'
 docker exec -i postgres-alyx-client sh -c 'chmod 600 .pgpass && chown postgres:postgres .pgpass'
