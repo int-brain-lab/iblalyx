03d059eff2ae7161a23768fad9af2875b76c1517
Miles Wells <k1o0@5tk.co>
2026-01-26 12:44:59 +0200

    Modify container location in alyx scripts

--- cortexlab/email.sh
+++ cortexlab/email.sh
@@ -1,5 +1,4 @@
-source ~/alyxvenv/bin/activate
-cd /var/www/alyx-main/alyx
+cd /var/www/alyx/alyx
 ./manage.py check_water_admin
 ./manage.py send_pending_notifications
 ./manage.py delete_expired_notifications
--- crons/cleanup_old_tasks.sh
+++ crons/cleanup_old_tasks.sh
@@ -1,9 +1,9 @@
-source /var/www/alyx-main/venv/bin/activate
+cd /var/www/alyx/alyx
 # Delete all tasks older than 2 years
 before_date=$(date -d "2 years ago" +"%Y-%m-%d")
-python /var/www/alyx-main/alyx/manage.py tasks cleanup --before $before_date
+python ./manage.py tasks cleanup --before $before_date
 # Delete complete tasks older than 2 months
 before_date=$(date -d "2 months ago" +"%Y-%m-%d")
-python /var/www/alyx-main/alyx/manage.py tasks cleanup --before $before_date --status complete
+python ./manage.py tasks cleanup --before $before_date --status complete
 # Delete tasks associated with signed-off sessions
-python /var/www/alyx-main/alyx/manage.py tasks cleanup --before $before_date --signed-off
+python ./manage.py tasks cleanup --before $before_date --signed-off
