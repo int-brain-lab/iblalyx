f1ea0708a48648d4d69c551ef3b7a4693e07ffd1
Georg Raiser <georg.raiser@internationalbrainlab.org>
2026-05-26 12:41:13 +0100

    Add subject bias history plot to overview subjects

--- management/ibl_reports/data_info.py
+++ management/ibl_reports/data_info.py
@@ -205,4 +205,5 @@ OVERVIEW_SUBJECT_PLOTS = [('subj_trial_count_session_duration', True),
                           ('subj_performance_easy_reaction_time', True),
                           ('subj_performance_heatmap', True),
                           ('subj_psychometric_curve', True),
+                          ('subject_bias_history_zero_contrast_bug', True),                          
                           ('subj_psychometric_fit_params', True)]
