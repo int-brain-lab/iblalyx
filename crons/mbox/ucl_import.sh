bcf6de354a2bf6e43765b1ee2b7da8e81d1abbe9
Miles Wells <k1o0@5tk.co>
2026-07-24 13:32:11 +0100

    Move release process to mbox Release now requires less than 1 GB of RAM, fewer manual steps, and is safer

--- crons/mbox/ucl_import.sh
+++ crons/mbox/ucl_import.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Create the database if it doesn't exist
-docker exec -i ucl_alyx_postgres psql -q -U ibl_dev -d postgres -c "CREATE DATABASE cortexlab;" 2>/dev/null || true
+docker exec -i cortexlab_buffer_postgres psql -q -U ibl_dev -d postgres -c "CREATE DATABASE cortexlab;" 2>/dev/null || true
 # Drop and recreate schema
-docker exec -i ucl_alyx_postgres psql -q -U ibl_dev -d cortexlab -c "drop schema public cascade; create schema public"
+docker exec -i cortexlab_buffer_postgres psql -q -U ibl_dev -d cortexlab -c "drop schema public cascade; create schema public"
 # Import the data from the remote alyx database into the local cortexlab database
 pg_dump -h alyx.cpd8pxc0pbj7.eu-west-2.rds.amazonaws.com -U alyx_admin -d alyx --no-owner --no-acl \
-| docker exec -i ucl_alyx_postgres psql -U ibl_dev -d cortexlab
+| docker exec -i cortexlab_buffer_postgres psql -U ibl_dev -d cortexlab
 # Ensure database migrated
 docker exec -i ibl_alyx_apache bash -c "./manage.py migrate --database cortexlab"
 # Prune imported data and import into IBL database
--- releases/01b_create_public_db.sh
+++ releases/01b_create_public_db.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 set -e
 
-# This script creates a local version of the public database through the following steps:
-# 1. Destroy the local instance of the public database
-# 2. Load the latest sql dump of the production database into the local public database
-# 3. Prune the local public database to contain only the to-be-released information
+# This script prunes the buffer database (already populated by 01a_download_database.sh) down
+# to only the to-be-released information. 02_upload_public_db.sh dumps the result: once to back
+# up to flatiron, once to load onto the real openalyx RDS.
 
 # Consult the Alyx playbook before running this, as it requires some careful setup. Minimally:
-# * Make sure you have the correct database set up as 'public' in the alyx installation you use
-# * Make sure all involved databases are on the same branch and commit (production, public, local install)
+# * Make sure production, buffer and openalyx are all on the same alyx branch/commit
 # * Record the output of this script in a log file (and submit these logs to github for the record later)
-# * Adjust the variables in the .env file according to your setup (see the .env_template file for the variables)
 
-# Source env variables and alyx env
-export $(grep -v '^#' .env | xargs)
-source $ALYX_VENV/bin/activate
+# Source env variables
+export $(grep -v '^#' ../docker/.env | xargs)
 
-# checking connection to database and settings
-python $ALYX_DIR/alyx/manage.py check
-python $ALYX_DIR/alyx/manage.py showmigrations
+echo "$(date '+%Y-%m-%d %H:%M:%S') Beginning to prune buffer database"
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') Beginning to create local version of public database"
+echo "... pruning buffer database"
+docker exec -i ibl_alyx_apache bash -c "python /home/iblalyx/releases/01b_prune_public_db.py"
 
-# Destroy local public database and load production database into it
-echo "... destroying local public database"
-psql -q -U $BUFFER_DB_USER -h $BUFFER_DB_HOST -d $BUFFER_DB_NAME -p $BUFFER_DB_PORT -c "drop schema public cascade"
-psql -q -U $BUFFER_DB_USER -h $BUFFER_DB_HOST -d $BUFFER_DB_NAME -p $BUFFER_DB_PORT -c "create schema public"
-echo "... rebuilding local public database from production"
-psql -q -U $BUFFER_DB_USER -h $BUFFER_DB_HOST -d $BUFFER_DB_NAME -p $BUFFER_DB_PORT -f "$TMP_DIR"/alyxfull.sql
-python $ALYX_DIR/alyx/manage.py makemigrations
-python $ALYX_DIR/alyx/manage.py migrate --database public
-
-# Prune local public database
-echo "... pruning local public database"
-python $ALYX_DIR/alyx/manage.py shell < 01b_prune_public_db.py
-# Export the public db to sql
-echo "... creating sql dump of local public database"
-/usr/bin/pg_dump -cOx -U $BUFFER_DB_USER -h $BUFFER_DB_HOST -d $BUFFER_DB_NAME -p $BUFFER_DB_PORT -f "$TMP_DIR"/openalyx.sql
-gzip -f -k "$TMP_DIR"/openalyx.sql
-## Copy sql as backup to mbox
-echo "... copying sql dump to mbox as backup"
-scp "$TMP_DIR"/openalyx.sql.gz "${SSH_STR[*]}":/backups/openalyx-backups/$(date +%Y-%m-%d)_openalyx.sql.gz
-echo "$(date '+%Y-%m-%d %H:%M:%S') Finished creating local version of public database"
+echo "$(date '+%Y-%m-%d %H:%M:%S') Finished pruning buffer database"
